a58cac5e373442fa1f2a0f8c5aff059cd0064850
Hassam Ali <hassam.ali@keeptruckin.com>
2020-02-21 01:57:47 +0500

    update login

--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,13 +13,14 @@ import { LoginService } from 'src/app/services/login.service';
 export class LoginComponent implements OnInit {
   validateForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private loginService: LoginService) { }
+  constructor(private router: Router, private fb: FormBuilder, private loginService: LoginService) { }
 
   submitForm(): void {
     if (this.validateForm.valid) {
       const userName = this.validateForm.controls.userName.value;
       const password = this.validateForm.controls.password.value;
       this.loginService.login(userName, password);
+      this.router.navigate(['home']);
     }
   }
 
