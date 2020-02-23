a470dec012a4bb55712e0a31d0208518caa9aa24
Hassam Ali <hassam.ali@keeptruckin.com>
2020-02-23 13:54:16 +0500

    watchlist

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,8 +10,15 @@ import { StockSearchComponent } from './stock-search/stock-search.component';
 import { StockGraphMiniComponent } from './stock-graph/stock-graph-mini/stock-graph-mini.component';
 import { LoginComponent } from './login/login.component';
 import { NewsCardComponent } from './news-card/news-card.component';
+import { WatchListCardComponent } from './watch-list-card/watch-list-card.component';
 
-const COMPONENTS = [StockCardComponent, StockSearchComponent, StockGraphMiniComponent, LoginComponent, NewsCardComponent];
+const COMPONENTS = [StockCardComponent,
+  StockSearchComponent,
+  StockGraphMiniComponent,
+  LoginComponent,
+  NewsCardComponent,
+  WatchListCardComponent,
+];
 
 @NgModule({
   declarations: [COMPONENTS],
---
+++ src/app/shared/watch-list-card/watch-list-card.component.ts
@@ -0,0 +1,15 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-watch-list-card',
+  templateUrl: 'watch-list-card.component.html',
+  styleUrls: ['watch-list-card.component.scss']
+})
+
+export class WatchListCardComponent implements OnInit {
+
+  private isUserLoggedIn: false;
+  ngOnInit(): void {
+  }
+}
