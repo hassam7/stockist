71c65839fd9895d4674ac5510afb905772ec2ff5
Hassam Ali <hassam.ali@keeptruckin.com>
2020-03-03 02:09:06 +0500

    add portfolio detail

--- src/app/portfolio/portfolio-routing.module.ts
+++ src/app/portfolio/portfolio-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PortfolioComponent } from './portfolio.component';
+import { PortfolioDetailComponent } from './portfolio-detail/portfolio-detail.component';
 
-const routes: Routes = [{ path: '', component: PortfolioComponent }];
+const routes: Routes = [
+  { path: '', component: PortfolioComponent },
+  { path: ':name', component: PortfolioDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class PortfolioRoutingModule {}
+export class PortfolioRoutingModule { }
--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PortfolioRoutingModule } from './portfolio-routing.module';
 import { PortfolioComponent } from './portfolio.component';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { PortfolioDetailComponent } from './portfolio-detail/portfolio-detail.component';
 
 @NgModule({
-  declarations: [PortfolioComponent],
+  declarations: [PortfolioComponent, PortfolioDetailComponent],
   imports: [CommonModule, PortfolioRoutingModule, NgZorroAntdModule]
 })
 export class PortfolioModule {}
