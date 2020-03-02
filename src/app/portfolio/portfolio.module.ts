bf7446e39d86ad1788b7c674b79d7cc898d01032
Hassam Ali <hassam.ali@keeptruckin.com>
2020-03-03 01:35:45 +0500

    add portfolio comopnent

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { PortfolioRoutingModule } from './portfolio-routing.module';
 import { PortfolioComponent } from './portfolio.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [PortfolioComponent],
-  imports: [CommonModule, PortfolioRoutingModule]
+  imports: [CommonModule, PortfolioRoutingModule, NgZorroAntdModule]
 })
 export class PortfolioModule {}
